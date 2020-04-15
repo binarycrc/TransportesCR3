af0d1d387afe68b1e0bca430ad8063ad24c1236f
binarycrc@gmail.com <binarycrc@gmail.com>
2020-04-15 11:01:22 -0600

    init

---
+++ Server/Server.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace Server
+{
+    partial class Server
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnServerStart = new System.Windows.Forms.Button();
+            this.lblServerStatus = new System.Windows.Forms.Label();
+            this.btnServerStop = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnServerStart
+            // 
+            this.btnServerStart.Location = new System.Drawing.Point(39, 51);
+            this.btnServerStart.Name = "btnServerStart";
+            this.btnServerStart.Size = new System.Drawing.Size(75, 23);
+            this.btnServerStart.TabIndex = 0;
+            this.btnServerStart.Text = "Iniciar";
+            this.btnServerStart.UseVisualStyleBackColor = true;
+            this.btnServerStart.Click += new System.EventHandler(this.btnServerStart_Click);
+            // 
+            // lblServerStatus
+            // 
+            this.lblServerStatus.AutoSize = true;
+            this.lblServerStatus.Location = new System.Drawing.Point(36, 130);
+            this.lblServerStatus.Name = "lblServerStatus";
+            this.lblServerStatus.Size = new System.Drawing.Size(90, 13);
+            this.lblServerStatus.TabIndex = 1;
+            this.lblServerStatus.Text = "Servidor detenido";
+            // 
+            // btnServerStop
+            // 
+            this.btnServerStop.Location = new System.Drawing.Point(39, 92);
+            this.btnServerStop.Name = "btnServerStop";
+            this.btnServerStop.Size = new System.Drawing.Size(75, 23);
+            this.btnServerStop.TabIndex = 2;
+            this.btnServerStop.Text = "Detener";
+            this.btnServerStop.UseVisualStyleBackColor = true;
+            this.btnServerStop.Click += new System.EventHandler(this.btnServerStop_Click);
+            // 
+            // Server
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnServerStop);
+            this.Controls.Add(this.lblServerStatus);
+            this.Controls.Add(this.btnServerStart);
+            this.Name = "Server";
+            this.Text = "Server";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnServerStart;
+        private System.Windows.Forms.Label lblServerStatus;
+        private System.Windows.Forms.Button btnServerStop;
+    }
+}
+
---
+++ Server/Server.cs
@@ -0,0 +1,97 @@
+﻿/*********************************************************************
+ * Copyright 2020 Pablo Ugalde
+ * Universidad Estatal A Distancia
+ * PRIMER CUATRI-2020 00830 PROGRAMACION AVANZADA
+ * 
+*********************************************************************/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Server
+{
+    public partial class Server : Form
+    {
+        TcpListener tcplistener;
+        Thread processClientListener;
+        bool serverStarted;
+        public Server()
+        {
+            InitializeComponent();
+            lblServerStatus.ForeColor = Color.Red;
+            btnServerStop.Enabled = false;
+        }
+
+        private void btnServerStart_Click(object sender, EventArgs e)
+        {
+            IPAddress localIPAddress = IPAddress.Parse("127.0.0.1");
+            tcplistener = new TcpListener(localIPAddress, 30000);
+            processClientListener = new Thread(new ThreadStart(ClientListener) );
+            processClientListener.Start();
+            processClientListener.IsBackground = true;
+
+            serverStarted = true;
+            lblServerStatus.Text = "Escuchando clientes en: " + localIPAddress.ToString();
+            lblServerStatus.ForeColor = Color.Green;
+            btnServerStart.Enabled = false;
+            btnServerStop.Enabled = true;
+
+        }
+        private void btnServerStop_Click(object sender, EventArgs e)
+        {
+            serverStarted = false;
+            lblServerStatus.Text = "Servidor detenido";
+            lblServerStatus.ForeColor = Color.Red;
+            btnServerStart.Enabled = true;
+            btnServerStop.Enabled = false;
+        }
+        private void ClientListener()
+        {
+            tcplistener.Start();
+            while (serverStarted)
+            {
+                TcpClient client = tcplistener.AcceptTcpClient();
+                Thread clientThread = new Thread(new ParameterizedThreadStart(clientComunication));
+                clientThread.Start(client);
+
+            }
+            //throw new NotImplementedException();
+        }
+
+        private void clientComunication(object obj)
+        {
+            TcpClient tcpClient = (TcpClient)obj;
+            NetworkStream clientStream = tcpClient.GetStream();
+            ASCIIEncoding encoder = new ASCIIEncoding();
+            byte[] buffer = new byte[4096];
+            int byteReaded;
+
+            while (serverStarted)
+            {
+                byteReaded = 0;
+                try
+                {
+                    byteReaded = clientStream.Read(buffer, 0, buffer.Length);
+                }
+                catch (Exception)
+                {
+                    break;
+                }
+                if (byteReaded == 0) { break; }
+                string message = encoder.GetString(buffer, 0, buffer.Length);
+                MessageBox.Show(string.Format("El cliente {0} se ha conectado", message),"Cliente conectado",MessageBoxButtons.OK,MessageBoxIcon.Information);
+            }
+            tcpClient.Close();
+            //throw new NotImplementedException();
+        }
+    }
+}
