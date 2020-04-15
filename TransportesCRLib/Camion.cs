1baf1ed4e8ba722b3355ed6a98cbe634e3c076a9
binarycrc@gmail.com <binarycrc@gmail.com>
2020-04-15 10:19:16 -0600

    Add project files.

---
+++ TransportesCRLib/Camion.cs
@@ -0,0 +1,63 @@
+﻿/*********************************************************************
+ * Copyright 2020 Pablo Ugalde
+ * Universidad Estatal A Distancia
+ * PRIMER CUATRI-2020 00830 PROGRAMACION AVANZADA
+ * 
+*********************************************************************/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TransportesCRLib
+{
+    class Camion
+    {
+        /// <summary>
+        /// Clase para el tipo de Camion
+        /// </summary>
+        string _placa;
+        string _modelo;
+        string _marca;
+        string _capacidadkilos;
+        string _capacidadvolumen;
+        /// <summary>
+        /// Constructor de la clase Camion
+        /// </summary>
+        /// <param name="placa"></param>
+        /// <param name="modelo"></param>
+        /// <param name="marca"></param>
+        /// <param name="capacidadkilos"></param>
+        /// <param name="capacidadvolumen"></param>
+        public Camion(string placa, string modelo, string marca, string capacidadkilos, string capacidadvolumen)
+        {
+            _placa = placa;
+            _modelo = modelo;
+            _marca = marca;
+            _capacidadkilos = capacidadkilos;
+            _capacidadvolumen = capacidadvolumen;
+        }
+
+        /// <summary>
+        /// Propiedad para el campo placa
+        /// </summary>
+        public string Placa { get { return _placa; } set { _placa = value; } }
+        /// <summary>
+        /// Propiedad para el campo Modelo
+        /// </summary>
+        public string Modelo { get { return _modelo; } set { _modelo = value; } }
+        /// <summary>
+        /// Propiedad para el campo Marca
+        /// </summary>
+        public string Marca { get { return _marca; } set { _marca = value; } }
+        /// <summary>
+        /// Propiedad del campo capacidad de kilos 
+        /// </summary>
+        public string capacidadkilos { get { return _capacidadkilos; } set { _capacidadkilos = value; } }
+        /// <summary>
+        /// Propiedad del campo capacidad de volumen
+        /// </summary>
+        public string capacidadvolumen { get { return _capacidadvolumen; } set { _capacidadvolumen = value; } }
+    }
+}
---
+++ TransportesCRLib/Conductor.cs
@@ -0,0 +1,63 @@
+﻿/*********************************************************************
+ * Copyright 2020 Pablo Ugalde
+ * Universidad Estatal A Distancia
+ * PRIMER CUATRI-2020 00830 PROGRAMACION AVANZADA
+ * 
+*********************************************************************/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TransportesCRLib
+{
+    class Conductor
+    {
+        /// <summary> 3
+        /// Clase base para registro para tipos de empleados
+        /// </summary>
+        string _identificacion;
+        string _nombre;
+        string _papellido;
+        string _sapellido;
+        string _ruta;
+        /// <summary>
+        /// Constructor de la clase Empleado
+        /// </summary>
+        /// <param name="identificacion"></param>
+        /// <param name="nombre"></param>
+        /// <param name="papellido"></param>
+        /// <param name="sapellido"></param>
+        /// <param name="rutaasignada"></param>
+        public Conductor(string identificacion, string nombre, string papellido, string sapellido, string rutaasignada)
+        {
+            _identificacion = identificacion;
+            _nombre = nombre;
+            _papellido = papellido;
+            _sapellido = sapellido;
+            _ruta = rutaasignada;
+        }
+        /// <summary>
+        /// Propiedad para el campo identificacon
+        /// </summary>
+        public string Identificacion { get { return _identificacion; } set { _identificacion = value; } }
+        /// <summary>
+        /// Propiedad para el campo nombre
+        /// </summary>
+        public string Nombre { get { return _nombre; } set { _nombre = value; } }
+
+        /// <summary>
+        /// Propiedad para el cambpo primer apellido
+        /// </summary>
+        public string PApellido { get { return _papellido; } set { _papellido = value; } }
+        /// <summary>
+        /// Propiedad para el campo segundo apellido
+        /// </summary>
+        public string SApellido { get { return _sapellido; } set { _sapellido = value; } }
+        /// <summary>
+        /// Propiedad del campo ruta
+        /// </summary>
+        public string Ruta { get { return _ruta; } set { _ruta = value; } }
+    }
+}
